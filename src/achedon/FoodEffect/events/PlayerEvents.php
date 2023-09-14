e813c8d8cff4096cb61c8691f64e94dd3de38690
leo deroin <leoderoinleo@gmail.com>
2023-09-14 15:09:49 +0200

    update to pm5

--- src/achedon/FoodEffect/events/PlayerEvents.php
+++ src/achedon/FoodEffect/events/PlayerEvents.php
@@ -8,16 +8,18 @@ use pocketmine\entity\effect\EffectInstance;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerItemConsumeEvent;
 
-class PlayerEvents implements Listener{
+class PlayerEvents implements Listener
+{
 
-    public function onConsume(PlayerItemConsumeEvent $event){
+    public function onConsume(PlayerItemConsumeEvent $event): void
+    {
         $player = $event->getPlayer();
         $item = $event->getItem();
         $ids = array_keys(Main::getInstance()->getConfig()->getAll());
-        if (in_array($item->getId(), $ids)){
-            $food = Main::getInstance()->getConfig()->getAll()[$item->getId()];
+        if (in_array($item->getName(), $ids)) {
+            $food = Main::getInstance()->getConfig()->getAll()[$item->getName()];
             $effect = $food["effect"];
-            foreach($effect as $id => $values){
+            foreach ($effect as $id => $values) {
                 $player->getEffects()->add(new EffectInstance(EffectIdMap::getInstance()->fromId($id), $values["duration"], $values["level"], $values["visible"]));
             }
         }
